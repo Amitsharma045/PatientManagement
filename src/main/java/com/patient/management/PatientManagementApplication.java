8cb2773ce2beb46d5db37474228b89f8311e312e
amit.sharma045 <amit.sharma045@gmail.com>
2021-03-29 22:23:23 +0530

    Revert "Removing everything, something new will come up soon"
    This reverts commit 8f19336920181f48f2524c7ec4f2901f4af1f82a.

---
+++ src/main/java/com/patient/management/PatientManagementApplication.java
@@ -0,0 +1,14 @@
+package com.patient.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementApplication {
+
+	public static void main(String[] args) {
+		System.out.println();
+		SpringApplication.run(PatientManagementApplication.class, args);
+	}
+
+}
